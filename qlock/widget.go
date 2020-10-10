a973f8ae3cfd0f4816dce1b4d86a41aa237eec54
Josh Komoroske <jdkomo@gmail.com>
2020-10-10 14:36:10 -0700

    Several small housekeeping tasks (#11)
    - Deleted unused screenshot
    - Readme badges
    - Readme license information
    - Renamed license to have .txt extention, and updated references

--- qlock/widget.go
+++ qlock/widget.go
@@ -1,6 +1,6 @@
 // Copyright Josh Komoroske. All rights reserved.
 // Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE.md file.
+// license that can be found in the LICENSE.txt file.
 
 package qlock
 
